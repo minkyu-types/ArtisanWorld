85a412e373245604937d76597789b587d6a1108a
its-mingyu <lion9638@naver.com>
2022-01-02 23:13:17 +0900

    [ 1차 ] - 레이아웃 구조 변경

--- app/src/main/java/com/architecture/artisanworld/MainActivity.java
+++ app/src/main/java/com/architecture/artisanworld/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_maker, R.id.navigation_wishlist)
                 .build();
-        NavHostFragment navHostFragment = (NavHostFragment)getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        NavHostFragment navHostFragment = (NavHostFragment)getSupportFragmentManager().findFragmentById(R.id.viewpager_nav_host);
 
         if(navHostFragment != null){
             NavController navController = NavHostFragment.findNavController(navHostFragment);
