6cb923f9d5e4eed56e2d43fa33f90feed0ddf0bc
its-mingyu <lion9638@naver.com>
2022-01-03 23:30:51 +0900

    [ 1차 ] - wishlist 수정
    [Have-to-do]
    - 파이어베이스 연동
    - 아이템 추가하기(gridlayoutManager)

---
+++ app/src/main/java/com/architecture/artisanworld/home/HomeAdapter.java
@@ -0,0 +1,17 @@
+package com.architecture.artisanworld.home;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class HomeAdapter extends RecyclerView.ViewHolder {
+
+    public HomeAdapter(@NonNull View itemView) {
+        super(itemView);
+    }
+
+    public void ViewHolder(){
+
+    }
+}
